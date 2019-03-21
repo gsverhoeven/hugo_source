984d5a4d0c2915566351ab11d00f7f33aa7aae91
gsverhoeven <gertjan.verhoeven@gmail.com>
2019-03-21 10:19:44 +0100

    Update source for hugo site (two new blog posts)

--- run_me_first.R
+++ run_me_first.R
@@ -5,10 +5,18 @@ blogdown::serve_site()
 # blogdown:::new_post_addin()
 #blogdown::new_post("newrmd", ext = '.Rmd')
 
-build_site()
+#build_site()
 
 # does not compile Rmds
-hugo_build()
+#hugo_build()
 
 # only runs hugo_build()
-serve_site()
+#serve_site()
+
+# After compilation, go to public folder repo. delete all current contents, except .git
+
+# copy the hugo generated public folder to the public folder repo.
+# Delete all .rds files (they  are huge)
+
+# Commit, it diffs automatically.
+# Push to github pages.
