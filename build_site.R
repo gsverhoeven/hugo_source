0f89dacacd5c9398a45f70405b8119b0b250cb4c
gsverhoeven <gertjan.verhoeven@gmail.com>
2021-03-07 12:51:47 +0100

    Update build_site.R

--- build_site.R
+++ build_site.R
@@ -1,7 +1,8 @@
 # this compiles all Rmds  and fills ./public in hugo_source
 
 #blogdown::build_site()
-blogdown:::build_rmds("content/post/2020-12-30-jacks_car_rental_gym.Rmd")
+#blogdown:::build_rmds("content/post/2020-12-30-jacks_car_rental_gym.Rmd")
+blogdown:::build_rmds("content/post/2021_03_07_qlearning_frozenlake_convergence.Rmd")
 blogdown::hugo_build()
 print("site build")
 
