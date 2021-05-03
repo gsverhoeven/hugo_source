4559d43212effbb163ca568093181212553d00ea
gsverhoeven <gertjan.verhoeven@gmail.com>
2021-05-03 14:40:36 +0200

    build zotero post

--- build_site.R
+++ build_site.R
@@ -3,6 +3,7 @@
 #blogdown::build_site()
 #blogdown:::build_rmds("content/post/2020-12-30-jacks_car_rental_gym.Rmd")
 #blogdown:::build_rmds("content/post/2021_03_07_qlearning_frozenlake_convergence.Rmd")
+#blogdown:::build_rmds("content/post/2021-05-02-workflow_scientific_papers.Rmd")
 blogdown::hugo_build()
 print("site build")
 
