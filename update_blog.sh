e57a4ff9219e94956200895725b6bcb34f4f0c16
gsverhoeven <gertjan.verhoeven@gmail.com>
2022-01-12 08:04:49 +0100

    update jws blog

--- update_blog.sh
+++ update_blog.sh
@@ -35,3 +35,23 @@ fi
 #Username for 'https://github.com': gsverhoeven
 #Password for 'https://gsverhoeven@github.com': PLAK hier het PAT uit Keepass
 
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "You need to specify the changes to commit as an argument"
+  exit 1
+else
+  git add -A
+  git commit -am "$1"
+  git push
+
+  hugo
+
+  cd public
+  git add -A
+  git commit -am "$1"
+  git push
+
+  cd ..
+fi
+
