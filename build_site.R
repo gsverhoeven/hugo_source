fa59deacbdcea7f91265a52886e1dc783065fea7
Gertjan <gertjan.verhoeven@gmail.com>
2025-04-30 09:23:51 +0200

    add ref to ancient hugo version needed to build site

--- build_site.R
+++ build_site.R
@@ -69,6 +69,9 @@ AND category Python for Python-bloggers
 # en uitzoeken hoe ik losse pngs automatisch in post/*-files/x.png krijg
 # dit geldt alleen voor figuren die niet door Rmarkdown chunks worden gemaakt
 
+# blogdown::install_hugo(version=0.63)
+options(blogdown.hugo.version = "0.63.0")
+
 # do blogdown:::build_rmds: only for new blog post
 
 # empty the public folder
@@ -88,7 +91,8 @@ AND category Python for Python-bloggers
 #blogdown:::build_rmds("content/post/2023-03-01_fumbbl_rosters_blood_bowl.Rmd")
 #blogdown:::build_rmds("content/post/2023-09-21_fumbbl_super_league.Rmd")
 #blogdown:::build_rmds("content/post/2024-01-15-stan_in_azure_vm.Rmd")
-blogdown:::build_rmds("content/post/2024-08-01_fumbbl_replays_blog_post.Rmd")
+#blogdown:::build_rmds("content/post/2024-08-01_fumbbl_replays_blog_post.Rmd")
+blogdown:::build_rmds("content/post/2025-04-29_fumbbl_league_team_development.Rmd")
 # rebuild site
 
 #The function blogdown::build_site() has an argument local that defaults to FALSE, which means building the website for publishing instead of local previewing
